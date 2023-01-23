57392bf7e843eee166201e15f3c36dd671082c0b
rmendes1 <joaorenatomendes@gmail.com>
2023-01-23 10:14:54 -0300

    onTitleChange, updateTodo, onCheckClick and onDelete added to methods

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,7 +4,10 @@
 border-gray-400 last:border-b-0">
                         <div class="flex items-center justify-center 
 mr-2">
-                            <button class="text-gray-400">
+                            <button :class="{'text-green-600': isCompleted,
+                                            'text-gray-400': !isCompleted
+                                            }"
+                                    @click="onCheckClick">
                                 <svg class="w-5 h-5" fill="none" 
 stroke="currentColor" viewBox="0 0 24 24" 
 xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" 
@@ -14,9 +17,10 @@ stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
 
                         <div class="w-full">
                             <input
+                                v-model="title"
+                                @keyup.enter="onTitleChange"
                                 type="text"
                                 placeholder="Digite a sua tarefa"
-                                :value="todo.title"
                                 class="bg-gray-300 placeholder-gray-500 
 text-gray-700 font-light focus:outline-none block w-full appearance-none 
 leading-normal mr-3"
@@ -25,7 +29,8 @@ leading-normal mr-3"
 
                         <div class="ml-auto flex items-center 
 justify-center">
-                            <button class="focus:outline-none">
+                            <button @click="onDelete"
+                                    class="focus:outline-none">
                                 <svg
                                     class="ml-3 h-4 w-4 text-gray-500"
                                     viewBox="0 0 24 24"
@@ -56,6 +61,45 @@ export default {
         todo: {
             type: Object,
             default: () => ({})
+        }
+    },
+
+    data() {
+        return {
+            title: this.todo.title,
+            isCompleted: this.todo.completed,
+        }
+    },
+
+    methods: {
+        onTitleChange() {
+
+            if (!this.title) {
+                return;
+            }
+
+            this.updateTodo()
+        },
+
+        updateTodo() {
+
+            const payload = {
+                id: this.todo.id,
+                data: {
+                    title: this.title,
+                    completed: this.todo.isCompleted
+                }
+            }
+            this.$store.dispatch('updateTodo', payload)
+        },
+
+        onCheckClick() {
+            this.isCompleted = !this.isCompleted
+            this.updateTodo()
+        },
+
+        onDelete() {
+            this.$store.dispatch('deleteTodo', this.todo.id)
         }
     }
 }
